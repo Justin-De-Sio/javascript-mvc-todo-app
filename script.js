0bfe8d4f758c146ebc7d055caef757fc5bffd671
Justin-De-Sio <desio.j@live.fr>
2022-03-07 11:32:43 +0100

    Model

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 class Model {
     constructor() {
+        // The state of the model, an array of to-do objects, prepopulated with some data
+        this.todos = [
+            {id: 1, text: 'Run a marathon', complete: false},
+            {id: 2, text: 'Plant a garden', complete: false},
+        ]
+    }
+
+    addTodo(todoText) {
+        const todo = {
+            id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+            text: todoText,
+            complete: false,
+        };
+
+        this.todos.push(todo)
+    }
+
+    // Map through all todos, and replace the text of the to-do with the specified id
+    editTodo(id, updatedText) {
+        this.todos = this.todos.map((todo) =>
+            todo.id === id ? {id: todo.id, text: updatedText, complete: todo.complete} : todo
+        )
+    }
+
+    // Filter a to-do out of the array by id
+    deleteTodo(id) {
+        this.todos = this.todos.filter((todo) => todo.id !== id)
+    }
+
+    // Flip the complete boolean on the specified to-do
+    toggleTodo(id) {
+        this.todos = this.todos.map((todo) =>
+            todo.id === id ? {id: todo.id, text: todo.text, complete: !todo.complete} : todo
+        )
+
     }
 }
 
@@ -8,11 +43,11 @@ class View {
     }
 }
 
-class Controller{
-    constructor(model,view) {
-        this.model = model;
-        this.view = view;
+class Controller {
+    constructor(model, view) {
+        this.model = model
+        this.view = view
     }
 }
 
-const app = new Controller(new Model(),new View());
+const app = new Controller(new Model(), new View())
