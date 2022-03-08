4f18885e0e9a700da8211dcea04f81bdfbf9c5dc
Justin-De-Sio <desio.j@live.fr>
2022-03-08 09:52:02 +0100

    localstorage

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 class Model {
     constructor() {
-        // The state of the model, an array of to-do objects, prepopulated with some data
-        this.todos = [
-            {id: 1, text: 'Run a marathon', complete: false},
-            {id: 2, text: 'Plant a garden', complete: false},
-        ]
+        // // The state of the model, an array of to-do objects, prepopulated with some data
+        // this.todos = [
+        //     {id: 1, text: 'Run a marathon', complete: false},
+        //     {id: 2, text: 'Plant a garden', complete: false},
+        // ]
+
+        this.todos = JSON.parse(localStorage.getItem('todos')) || []
     }
 
     addTodo(todoText) {
@@ -15,7 +17,7 @@ class Model {
         };
 
         this.todos.push(todo)
-        this.onTodoListChanged(this.todos)
+        this._commit(this.todos)
     }
 
     // Map through all todos, and replace the text of the to-do with the specified id
@@ -23,13 +25,13 @@ class Model {
         this.todos = this.todos.map((todo) =>
             todo.id === id ? {id: todo.id, text: updatedText, complete: todo.complete} : todo
         )
-        this.onTodoListChanged(this.todos)
+        this._commit(this.todos)
     }
 
     // Filter a to-do out of the array by id
     deleteTodo(id) {
         this.todos = this.todos.filter((todo) => todo.id !== id)
-        this.onTodoListChanged(this.todos)
+        this._commit(this.todos)
     }
 
     // Flip the complete boolean on the specified to-do
@@ -37,10 +39,17 @@ class Model {
         this.todos = this.todos.map((todo) =>
             todo.id === id ? {id: todo.id, text: todo.text, complete: !todo.complete} : todo
         )
-        this.onTodoListChanged(this.todos)
+        this._commit(this.todos)
+
     }
-    bindTodoListChanged(callback){
+
+    bindTodoListChanged(callback) {
         this.onTodoListChanged = callback
+    }
+
+    _commit(todos) {
+        this.onTodoListChanged(this.todos)
+        localStorage.setItem('todos', JSON.stringify(todos))
     }
 }
 
